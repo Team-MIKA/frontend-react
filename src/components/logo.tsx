5d197e492511695e642ba012549c08386c8ecb9e
Mads Smed <ms@humanrisks.com>
2021-11-08 08:49:01 +0100

    Remove a-tag

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -25,19 +25,17 @@ const Logo: FC = () => {
     const cloudLogoPath = `/logo.svg`; //TODO ${useColorModeValue("", "-dark")} Make dark mode logo
     return (
         <Link href="/">
-            <a>
-                <LogoBox>
-                    <Image src={cloudLogoPath} width={20} height={20} alt="logo" />
-                    <Text
-                        color={useColorModeValue("gray.800", "whiteAlpha.900")}
-                        fontFamily='M PLUS Rounded 1c", sans-serif'
-                        fontWeight="bold"
-                        ml={3}
-                    >
-                        Team Mika
-                    </Text>
-                </LogoBox>
-            </a>
+            <LogoBox>
+                <Image src={cloudLogoPath} width={20} height={20} alt="logo" />
+                <Text
+                    color={useColorModeValue("gray.800", "whiteAlpha.900")}
+                    fontFamily='M PLUS Rounded 1c", sans-serif'
+                    fontWeight="bold"
+                    ml={3}
+                >
+                    Team Mika
+                </Text>
+            </LogoBox>
         </Link>
     );
 };
