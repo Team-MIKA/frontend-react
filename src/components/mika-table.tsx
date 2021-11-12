759de729c0c2c8660864dc6f9fb8051f3d788e45
Kaare Hattel <kaare.hattel@gmail.com>
2021-11-12 10:26:52 +0100

    V1: MikaTable

--- src/components/mika-table.tsx
+++ src/components/mika-table.tsx
@@ -1,50 +1,14 @@
-import { Table, Thead, Tbody, Tr, Th, Td, chakra, TableCaption } from "@chakra-ui/react"
-import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons"
-import { useTable, useSortBy } from "react-table"
+import { Table, Thead, Tbody, Tr, Th, Td, TableCaption, Container, Box } from "@chakra-ui/react"
+import { useTable } from "react-table"
 
-import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css';
-import 'primereact/resources/primereact.css';
 
 import React, {useState, useRef} from "react"
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { Toast } from 'primereact/toast';
 
 const MikaTable = ({columns, data, title}) => {
-
-    /* const [selectedItem, setSelectedItem] = useState(null);
-    const toast = useRef(null);
-
-    const dynamicColumns = columns.map((col,i) => {
-        return <Column key={col.field} field={col.field} header={col.header} />;
-    });
-
-    
-
-    const onRowUnselect = (event) => {
-        toast.current.show({ severity: 'warn', summary: 'Item Unselected', detail: `Name: ${event.data.title}`, life: 3000 });
-    }
-
-    return (
-        <div>
-            <Toast ref={toast} />
-            <div>
-                <DataTable value={data} header={title} 
-                metaKeySelection={false}
-                responsiveLayout="scroll" selectionMode="single" selection={selectedItem} 
-                onSelectionChange={e => setSelectedItem(e.value)} 
-                dataKey="id" 
-                onRowSelect={onRowSelect} onRowUnselect={onRowUnselect}>
-                    {dynamicColumns}
-                </DataTable> 
-            </div>
-        </div>
-             
-    
-    ); */
-
+    const [selectedItem, setSelectedItem] = useState(null);
 
     const onRowSelect = (event) => {
+        selectedItem == event.id ? setSelectedItem(null) : setSelectedItem(event.id)
         console.log(event)
     }
 
@@ -54,51 +18,39 @@ const MikaTable = ({columns, data, title}) => {
     headerGroups,
     rows,
     prepareRow,
-  } = useTable({ columns, data }, useSortBy)
+  } = useTable({ columns, data })
 
      return (
-         <div> 
-            <Table {...getTableProps()} >
+         <Box  border="2px solid gray" borderRadius="md">
+            <Table {...getTableProps()}  >
                 <TableCaption>{title}</TableCaption>
-            <Thead>
-                {headerGroups.map((headerGroup) => (
-                <Tr {...headerGroup.getHeaderGroupProps()}>
-                    {headerGroup.headers.map((column) => (
-                    <Th
-                        {...column.getHeaderProps(column.getSortByToggleProps())}
-                    >
-                        {column.render("Header")}
-                        <chakra.span pl="4">
-                        {column.isSorted ? (
-                            column.isSortedDesc ? (
-                            <TriangleDownIcon aria-label="sorted descending" />
-                            ) : (
-                            <TriangleUpIcon aria-label="sorted ascending" />
-                            )
-                        ) : null}
-                        </chakra.span>
-                    </Th>
-                    ))}
-                </Tr>
-                ))}
-            </Thead>
-            <Tbody {...getTableBodyProps()}>
-                {rows.map((row) => {
-                prepareRow(row)
-                return (
-                    <Tr {...row.getRowProps()} onClick={(() => onRowSelect(row))} style={{cursor: 'pointer'}}>
-                    {row.cells.map((cell) => (
-                        <Td {...cell.getCellProps()}>
-                        {cell.render("Cell")}
-                        </Td>
-                    ))}
+                <Thead>
+                    {headerGroups.map((headerGroup) => (
+                    <Tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+                        <Th {...column.getHeaderProps()}>
+                            {column.render("Header")}
+                        </Th>
+                        ))}
                     </Tr>
-                )
-                })}
-            </Tbody>
+                    ))}
+                </Thead>
+                <Tbody {...getTableBodyProps()}>
+                    {rows.map((row) => {
+                    prepareRow(row)
+                    return (
+                        <Tr {...row.getRowProps()} onClick={(() => onRowSelect(row))} style={{cursor: 'pointer', background: row.id == selectedItem ? "gray" : ''}}>
+                        {row.cells.map((cell) => (
+                            <Td {...cell.getCellProps()}>
+                                {cell.render("Cell")}
+                            </Td>
+                        ))}
+                        </Tr>
+                    )
+                    })}
+                </Tbody>
             </Table>
-        </div>
-       
+        </Box>
       )
     
 };
