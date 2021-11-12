feaea4d9cbe01ff82cbd98d31a3ec409ceea0002
Kaare Hattel <kaare.hattel@gmail.com>
2021-11-12 09:20:26 +0100

    Adding test site and data

--- src/pages/musk.tsx
+++ src/pages/musk.tsx
@@ -1,12 +1,38 @@
 import { Container, Heading } from "@chakra-ui/react";
 import { NextPage } from "next";
 import MikaTable from "@components/mika-table";
+import React from "react";
 
 const Musk: NextPage = () => {
+
+    const data = React.useMemo(
+        () => [
+          { index: "1", title: "Order1" },
+          { index: "2", title: "Order2" },
+          { index: "3", title: "Order3" },
+        ],
+        [],
+      )
+    
+      const columns = React.useMemo(
+        () => [
+          {
+             Header: "Index",
+            accessor: "index",
+          },
+          {
+            Header: "Title",
+            accessor: "title",
+          },
+        ],
+        [],
+      )
+
+
     return (
         <Container>
             <Heading>Test Site</Heading>
-            <MikaTable></MikaTable>
+            <MikaTable columns={columns} data={data} title="Orders"></MikaTable>
         </Container>
     );
 };
