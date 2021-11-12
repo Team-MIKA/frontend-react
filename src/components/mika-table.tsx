8bbaa7158bb988dfa6bc0b6d08b42408d661c702
Kaare Hattel <kaare.hattel@gmail.com>
2021-11-12 17:01:35 +0100

    Now complies to strong Type Rules :japanese_goblin:

--- src/components/mika-table.tsx
+++ src/components/mika-table.tsx
@@ -1,89 +1,100 @@
-import { Table, Thead, Tbody, Tr, Th, Td, TableCaption, Box } from "@chakra-ui/react"
-import { useToast } from "@chakra-ui/react"
-import { useTable } from "react-table"
+import React, { useState, useEffect } from "react";
+import { Table, Thead, Tbody, Tr, Th, Td, TableCaption, Box } from "@chakra-ui/react";
+import { useToast } from "@chakra-ui/react";
+import { Column, Row, useTable } from "react-table";
 
-import React, {useState, useEffect} from "react"
-import itemStore from "@store/item";
 import logger from "@helpers/logger";
+import itemStore from "@store/item";
 
-const MikaTable = ({columns, data, title}) => {
-
+const MikaTable = ({ columns, data, title }: { columns: Column<JSON>[]; data: JSON[]; title: string }) => {
     const toast = useToast();
 
     const [itemState, setItemState] = useState(itemStore.initialState);
-    
+
     useEffect(() => {
         const subscription = itemStore.subscribe(setItemState);
-        logger.log("Initial Itemstate:", itemState);
+        logger.log("Initial Item state:", itemState);
         return () => subscription.unsubscribe();
-    }, []); // [] avoids useEffect to be run on every render of component.
+    }, [itemState]); // [] avoids useEffect to be run on every render of component.
 
-    const onRowClick = (event) => {
+    const onRowClick = (event: Row<JSON>) => {
         logger.log("Row Clicked: ", event);
-        itemState.item.id == event.original.id ? unSelect() : select(event.original.id);
-    }
+        itemState.item.id == event.values.id ? unSelect() : select(event.values.id);
+    };
 
     const unSelect = () => {
         logger.log("UnSelecting...");
         itemStore.clearItem();
         addUnselectToast();
-    }
+    };
 
-    const select = (itemId) => {
+    const select = (itemId: string) => {
         logger.log("Selecting ID: ", itemId);
-        itemStore.setItem({id: itemId});
+        itemStore.setItem({ id: itemId });
         addSelectToast();
-    }
+    };
 
-    const addSelectToast = () =>  {
-        toast({description: "Item Selected", position: 'top-right', duration: 2000, isClosable: true, status: 'success'});
-    }
+    const addSelectToast = () => {
+        toast({
+            description: "Item Selected",
+            position: "top-right",
+            duration: 2000,
+            isClosable: true,
+            status: "success",
+        });
+    };
 
-    const addUnselectToast = () =>  {
-        toast({description: "Item Unselected", position: 'top-right', duration: 2000, isClosable: true, status: 'warning'});
-    }
+    const addUnselectToast = () => {
+        toast({
+            description: "Item Unselected",
+            position: "top-right",
+            duration: 2000,
+            isClosable: true,
+            status: "warning",
+        });
+    };
 
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
 
-    const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({ columns, data });
-
-     return (
-         <Box  border="2px solid gray" borderRadius="md">
-            <Table {...getTableProps()}  >
+    return (
+        <Box border="2px solid gray" borderRadius="md">
+            <Table {...getTableProps()}>
                 <TableCaption>{title}</TableCaption>
                 <Thead>
                     {headerGroups.map((headerGroup) => (
-                    <Tr {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map((column) => (
-                        <Th {...column.getHeaderProps()}>
-                            {column.render("Header")}
-                        </Th>
-                        ))}
-                    </Tr>
+                        <Tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
+                            {headerGroup.headers.map((column) => (
+                                <Th {...column.getHeaderProps()} key={column.id}>
+                                    {column.render("Header")}
+                                </Th>
+                            ))}
+                        </Tr>
                     ))}
                 </Thead>
                 <Tbody {...getTableBodyProps()}>
                     {rows.map((row) => {
-                    prepareRow(row)
-                    return (
-                        <Tr {...row.getRowProps()} onClick={(() => onRowClick(row))} style={{cursor: 'pointer', background: row.original.id == itemState.item.id ? "gray" : ''}}>
-                        {row.cells.map((cell) => (
-                            <Td {...cell.getCellProps()}>
-                                {cell.render("Cell")}
-                            </Td>
-                        ))}
-                        </Tr>
-                    )
+                        prepareRow(row);
+                        return (
+                            <Tr
+                                {...row.getRowProps()}
+                                onClick={() => onRowClick(row)}
+                                style={{
+                                    cursor: "pointer",
+                                    background: row.values.id == itemState.item.id ? "gray" : "",
+                                }}
+                                key={row.id}
+                            >
+                                {row.cells.map((cell) => (
+                                    <Td {...cell.getCellProps()} key={cell.row.id + cell.column.id}>
+                                        {cell.render("Cell")}
+                                    </Td>
+                                ))}
+                            </Tr>
+                        );
                     })}
                 </Tbody>
             </Table>
         </Box>
-      )
-    
+    );
 };
 export default MikaTable;
