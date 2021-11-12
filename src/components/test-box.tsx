1cd8e63682215b49e75cf536410b7265b9566e91
Kaare Hattel <kaare.hattel@gmail.com>
2021-11-12 17:01:39 +0100

    Now complies to strong Type Rules :japanese_goblin:

--- src/components/test-box.tsx
+++ src/components/test-box.tsx
@@ -1,25 +1,21 @@
-import {Box, Text, Heading } from "@chakra-ui/react"
+import React, { useState, useEffect } from "react";
+import { Box, Text, Heading } from "@chakra-ui/react";
 import itemStore from "@store/item";
-import React, {useState, useEffect} from "react"
 
-
-const TestBox = ({title}) => {
-
+const TestBox = ({ title }: { title: string }) => {
     const [itemState, setItemState] = useState(itemStore.initialState);
 
     useEffect(() => {
         const subscription = itemStore.subscribe(setItemState);
         return () => subscription.unsubscribe();
-    }, []); // [] avoids useEffect to be run on every render of component.
+    }, [itemState]); // [] avoids useEffect to be run on every render of component.
 
-    return(
-        <Box border="2px solid gray" borderRadius="md"> 
+    return (
+        <Box border="2px solid gray" borderRadius="md">
             <Heading> {title}</Heading>
-            <Text > 
-                {itemState.item.id}
-            </Text>
+            <Text>{itemState.item.id}</Text>
         </Box>
-    )
+    );
 };
 
 export default TestBox;
