ab18bcf733e8c9d8db33886c7aad81de1fe83854
Kaare Hattel <kaare.hattel@gmail.com>
2021-11-12 17:01:41 +0100

    Now complies to strong Type Rules :japanese_goblin:

--- src/pages/musk.tsx
+++ src/pages/musk.tsx
@@ -1,35 +1,34 @@
+import React from "react";
 import { Container, Heading } from "@chakra-ui/react";
 import { NextPage } from "next";
+import { Column } from "react-table";
 import MikaTable from "@components/mika-table";
-import React from "react";
 import TestBox from "@components/test-box";
 
 const Musk: NextPage = () => {
-
     const data = React.useMemo(
         () => [
-          { id: "1234", title: "Batman" },
-          { id: "5678", title: "Superman" },
-          { id: "9101", title: "The Joker" },
+            JSON.parse('{ "id": "1234", "title": "Batman" }'),
+            JSON.parse('{ "id": "5678", "title": "Superman" }'),
+            JSON.parse('{ "id": "9101", "title": "The Joker" }'),
         ],
-        [],
-      )
-    
-      const columns = React.useMemo(
-        () => [
-          { Header: "Id", accessor: "id" },
-          { Header: "Title", accessor: "title" },
-        ],
-        [],
-      )
+        []
+    );
 
+    const columns = React.useMemo(
+        () => [
+            { Header: "Id", accessor: "id" } as Column<JSON>,
+            { Header: "Title", accessor: "title" } as Column<JSON>,
+        ],
+        []
+    );
 
     return (
         <Container>
             <Heading right="0">Test Site</Heading>
-            <MikaTable columns={columns} data={data} title="Orders"></MikaTable>
-            <TestBox title="Sap Materials"></TestBox>
-            <TestBox title="Time Smart"></TestBox>
+            <MikaTable columns={columns} data={data} title={"Orders"} />
+            <TestBox title="Sap Materials" />
+            <TestBox title="Time Smart" />
         </Container>
     );
 };
