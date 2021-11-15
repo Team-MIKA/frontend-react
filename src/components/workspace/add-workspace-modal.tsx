689ff2872216ce742f51d4dc0e5523c5e540682e
Kaare Hattel <kaare.hattel@gmail.com>
2021-11-15 21:42:04 +0100

    Workspace modal V1

--- src/components/workspace/add-workspace-modal.tsx
+++ src/components/workspace/add-workspace-modal.tsx
@@ -1,60 +1,68 @@
-import { 
-    useDisclosure, 
-    Button, 
-    Modal, 
-    ModalOverlay, 
-    ModalCloseButton, 
-    ModalHeader, 
-    ModalBody, 
-    ModalFooter, 
+import { useState } from "react";
+import {
+    useDisclosure,
+    Button,
+    Modal,
+    ModalOverlay,
+    ModalCloseButton,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
     ModalContent,
     Input,
-    Text } from "@chakra-ui/react"
+    Text,
+} from "@chakra-ui/react";
+import { useRecoilState } from "recoil";
+import { WorkspaceState } from "@store";
 
-import {useState} from "react"
+const AddWorkspaceModal = () => {
+    const [title, setTitle] = useState("");
 
-const AddWorkspace = () =>  {
+    const [workspaces, setWorkspaces] = useRecoilState(WorkspaceState);
 
-    const [value, setValue]  = useState("")
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     const save = () => {
-        console.log("SAVE WORKSPACE: ", value);
+        // Save to database
+        // Insert generated ID from backend
+        // Add to state
+
+        //Mock db returning ID.
+        const idFromDb = Math.floor(Math.random() * 16).toString();
+
+        setWorkspaces([...workspaces, { title: title, id: idFromDb }]);
+        setTitle("");
         return onClose();
-    }
+    };
 
     return (
-      <>
-        <Button onClick={onOpen}>Add Workspace</Button>
-  
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Add Workspace</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
+        <>
+            <Button onClick={onOpen}>Add Workspace</Button>
 
-                <Text mb="8px">Value: {value}</Text>
-                <Input
-                    value={value}
-                    onChange={ (event) => setValue(event.target.value)}
-                    placeholder="Title of workspace"
-                    size="sm"
-                />
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Add Workspace</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Text mb="8px">Value: {title}</Text>
+                        <Input
+                            value={title}
+                            onChange={(event) => setTitle(event.target.value)}
+                            placeholder="Title of workspace"
+                            size="sm"
+                        />
+                    </ModalBody>
 
-            </ModalBody>
-  
-            <ModalFooter>
-              <Button colorScheme="blue" mr={3} onClick={save}>
-                Save
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3} disabled={title == ""} onClick={save}>
+                            Save
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    );
+};
 
-
-      </>
-    )
-  }
-
-  export default AddWorkspace
+export default AddWorkspaceModal;
