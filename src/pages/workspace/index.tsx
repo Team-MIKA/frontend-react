70316a66d4f4b6bfb380f62703cddb9b53fec238
Kaare Hattel <kaare.hattel@gmail.com>
2021-11-15 22:51:06 +0100

    REmoved container tag

--- src/pages/workspace/index.tsx
+++ src/pages/workspace/index.tsx
@@ -1,18 +1,18 @@
-import { Box, Container } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { NextPage } from "next";
 import AddWorkspaceModal from "@components/workspace/add-workspace-modal";
 import WorkspaceList from "@components/workspace/workspace-list";
 
 const WorkspacePage: NextPage = () => {
     return (
-        <Container>
+        <>
             <Box mb={4}>
                 <WorkspaceList />
             </Box>
             <Box>
                 <AddWorkspaceModal />
             </Box>
-        </Container>
+        </>
     );
 };
 
