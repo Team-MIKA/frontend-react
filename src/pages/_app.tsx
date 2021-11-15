913d5d58d22a58c2bb5362dd380da22a95d84723
Kaare Hattel <kaare.hattel@gmail.com>
2021-11-15 12:38:04 +0100

    Fixed import placement

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
+import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AnimatePresence } from "framer-motion";
 import { AppProps } from "next/app";
-import React from "react";
 import { RecoilRoot } from "recoil";
 import Fonts from "../components/fonts";
 import Layout from "../components/layouts/main";
