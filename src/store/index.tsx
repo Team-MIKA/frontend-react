ba0f0bd1cbb1995caaac0c7be491fdf3ce011b9c
Kaare Hattel <kaare.hattel@gmail.com>
2021-11-15 21:43:46 +0100

    Added workspaceState

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -4,3 +4,13 @@ export const GroupMemberState = atom({
     key: "groupMembers",
     default: [] as string[],
 });
+
+export interface Workspace {
+    id: string;
+    title: string;
+}
+
+export const WorkspaceState = atom({
+    key: "workspaces",
+    default: [] as Workspace[],
+});
