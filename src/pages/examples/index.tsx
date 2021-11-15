64a50f73ad87a053b11a55eb059ec8b257c70cfe
Kaare Hattel <kaare.hattel@gmail.com>
2021-11-15 21:46:50 +0100

    Added "NextPage"

--- src/pages/examples/index.tsx
+++ src/pages/examples/index.tsx
@@ -1,7 +1,8 @@
+import { NextPage } from "next";
 import AddGroupMember from "@components/examples/add-group-member";
 import GroupList from "@components/examples/group-list";
 
-const Example = () => {
+const Example: NextPage = () => {
     return (
         <>
             <AddGroupMember />
