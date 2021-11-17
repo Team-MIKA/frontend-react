dd4a7183aabfa0998ab803d331df539a658696fc
Anders Aaen Springborg <aaspringborg@gmail.com>
2021-11-17 11:19:07 +0100

    add test

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,17 @@
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { compilerOptions } = require("./tsconfig.json");
+
 module.exports = {
-    collectCoverageFrom: ["**/*.{js,jsx,ts,tsx}", "!**/*.d.ts", "!**/node_modules/**"],
-    moduleNameMapper: {
-        // Handle CSS imports (with CSS modules)
-        // https://jestjs.io/docs/webpack#mocking-css-modules
-        "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
-
-        // Handle CSS imports (without CSS modules)
-        "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
-
-        // Handle image imports
-        // https://jestjs.io/docs/webpack#handling-static-assets
-        "^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$": `<rootDir>/__mocks__/fileMock.js`,
-
-        // Handle module aliases
-        "^@/components/(.*)$": "<rootDir>/components/$1",
-    },
+    collectCoverageFrom: ["**/*.{ts,tsx}", "!**/*.d.ts", "!**/node_modules/**"],
+    moduleDirectories: ["node_modules", "src/"],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/src/" }),
     setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
     testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
     transform: {
         // Use babel-jest to transpile tests with the next/babel preset
         // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
-        "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+        "^.+\\.(ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
     },
     transformIgnorePatterns: ["/node_modules/", "^.+\\.module\\.(css|sass|scss)$"],
+    testEnvironment: "jest-environment-jsdom",
 };
