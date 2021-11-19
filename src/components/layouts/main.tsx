db34fdc80cbf7af0a2564ea68ad69fa2fcd69bb4
Anders Aaen Springborg <aaspringborg@gmail.com>
2021-11-19 11:51:26 +0100

    move navbar items

--- src/components/layouts/main.tsx
+++ src/components/layouts/main.tsx
@@ -1,17 +1,20 @@
 import { Box, Container } from "@chakra-ui/react";
 import { NextPage } from "next";
 import Head from "next/head";
+import { NavBarItem, TextNavItem } from "@components/layouts/navbar/navbaritem";
 import Footer from "./footer";
 import NavBar from "./navbar/navbar";
 
 const Main: NextPage = ({ children }) => {
+    const Items: NavBarItem[] = [new TextNavItem("/", "Home"), new TextNavItem("/settings", "Settings")];
+
     return (
         <Box as="main" pb={8}>
             <Head>
                 <title>Business assistant</title>
             </Head>
 
-            <NavBar />
+            <NavBar navItems={Items} />
 
             <Container maxW="container.md" pt={14}>
                 {children}
