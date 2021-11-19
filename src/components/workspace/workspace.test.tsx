c6017398d31260d8dcf0fc0b1dea44c36d143f88
Kaare Hattel <kaare.hattel@gmail.com>
2021-11-19 14:08:02 +0100

    Added tests for workspace

--- src/components/workspace/workspace.test.tsx
+++ src/components/workspace/workspace.test.tsx
@@ -1,13 +1,17 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import Router from "next/router";
 import { RecoilRoot } from "recoil";
-import { Workspace } from "@store/index";
+import { Workspace, WorkspaceListState } from "@store/index";
 import DeleteWorkspaceModal from "./delete-workspace-modal";
 import WorkspaceCard from "./workspace-card";
+import WorkspaceList from "./workspace-list";
+
+jest.mock("next/dist/client/router", () => require("next-router-mock"));
 
 describe("WorkspaceCard", () => {
     const workspace = { title: "test", id: "123" } as Workspace;
+
     beforeEach(() => {
         render(
             <RecoilRoot>
@@ -21,17 +25,16 @@ describe("WorkspaceCard", () => {
         expect(heading).toHaveTextContent(workspace.title);
     });
 
+    test("When clicking card, url changes", () => {
+        const link = screen.getByText(workspace.title);
+        userEvent.click(link);
+
+        expect(Router).toMatchObject({ asPath: "/workspace/" + workspace.id });
+    });
+
     test("Have delete workspace button", () => {
         const button = screen.getByTestId("removeWorkspace");
         expect(button).toBeInTheDocument;
-    });
-
-    test("click delete button renders modal", () => {
-        const button = screen.getByTestId("removeWorkspace");
-        userEvent.click(button);
-
-        const modal = screen.getByTestId("deleteWorkspaceModal");
-        expect(modal).toBeVisible;
     });
 });
 
@@ -55,5 +58,37 @@ describe("DeleteWorkspaceModal", () => {
     test("Have delete button", () => {
         const deleteBtn = screen.getByRole("delete");
         expect(deleteBtn).toBeInTheDocument;
+    });
+
+    test("Display workspace title", () => {
+        const workspaceTitle = screen.findByText("delete" + workspace.title);
+        expect(workspaceTitle).toBeInTheDocument;
+    });
+});
+
+describe("WorkspaceList", () => {
+    const workspace1 = { title: "hest", id: "123" } as Workspace;
+    const workspace2 = { title: "ged", id: "321" } as Workspace;
+
+    const initializeState = ({ set }: any) => {
+        set(WorkspaceListState, [workspace1, workspace2]);
+    };
+
+    beforeEach(() => {
+        render(
+            <RecoilRoot initializeState={initializeState}>
+                <WorkspaceList></WorkspaceList>
+            </RecoilRoot>
+        );
+    });
+
+    test("Display title of workspace1", () => {
+        const workspaceTitle1 = screen.findByText(workspace1.title);
+
+        expect(workspaceTitle1).toBeInTheDocument;
+    });
+    test("Display title of workspace2", () => {
+        const workspaceTitle2 = screen.findByText(workspace2.title);
+        expect(workspaceTitle2).toBeInTheDocument;
     });
 });
