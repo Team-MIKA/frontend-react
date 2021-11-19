69d8e7c31b220ce889ffcf75908175d19cd7e6cc
Kaare Hattel <kaare.hattel@gmail.com>
2021-11-19 08:52:48 +0100

    WIP: Delete button

--- src/components/workspace/workspace-card.tsx
+++ src/components/workspace/workspace-card.tsx
@@ -1,21 +1,37 @@
-import { Stack, Heading, Text, useColorModeValue } from "@chakra-ui/react";
+import { DeleteIcon } from "@chakra-ui/icons";
+import { Stack, Heading, Text, useColorModeValue, IconButton } from "@chakra-ui/react";
 import Link from "next/link";
-import { Workspace } from "@store";
+import { useRecoilState } from "recoil";
+import { Workspace, WorkspaceListState } from "@store";
 
 const WorkspaceCard = ({ workspace }: { workspace: Workspace }) => {
+    const [workspaces, setWorkspaces] = useRecoilState(WorkspaceListState);
+
+    const removeWorkspace = () => {
+        const idx = workspaces.findIndex((x) => x.id == workspace.id);
+        setWorkspaces([...workspaces.splice(idx, 1)]);
+    };
+
     return (
         <>
-            <Link href={"/workspace/" + workspace.id} passHref>
-                <Stack
-                    boxShadow={"xl"}
-                    bg={useColorModeValue("gray.50", "gray.700")}
-                    rounded={"xl"}
-                    p={10}
-                    spacing={8}
-                    align={"center"}
-                    w={"full"}
-                    _hover={{ bg: useColorModeValue("gray.100", "gray.800") }}
-                >
+            <Stack
+                boxShadow={"xl"}
+                bg={useColorModeValue("gray.50", "gray.700")}
+                rounded={"xl"}
+                p={10}
+                spacing={8}
+                align={"center"}
+                w={"full"}
+                _hover={{ bg: useColorModeValue("gray.100", "gray.800") }}
+            >
+                <IconButton
+                    background={"transparent"}
+                    icon={<DeleteIcon />}
+                    aria-label={"Remove Workspace"}
+                    onClick={() => removeWorkspace}
+                />
+
+                <Link href={"/workspace/" + workspace.id} passHref>
                     <Stack align={"center"} spacing={2}>
                         <Heading
                             textTransform={"uppercase"}
@@ -26,8 +42,8 @@ const WorkspaceCard = ({ workspace }: { workspace: Workspace }) => {
                         </Heading>
                         <Text fontSize={"lg"} color={"gray.500"}></Text>
                     </Stack>
-                </Stack>
-            </Link>
+                </Link>
+            </Stack>
         </>
     );
 };
