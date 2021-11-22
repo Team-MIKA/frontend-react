6b7e2893d40b0fec5fda9504c2260c0c6132ee13
Ida <ichris18@student.aau.dk>
2021-11-22 14:45:31 +0100

    Made layout dynamic, so more widgets can be seen by the user

--- src/components/layouts/main.tsx
+++ src/components/layouts/main.tsx
@@ -1,4 +1,4 @@
-import { Box, Container } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { NextPage } from "next";
 import Head from "next/head";
 import Footer from "./footer";
@@ -13,10 +13,10 @@ const Main: NextPage = ({ children }) => {
 
             <NavBar />
 
-            <Container maxW="container.md" pt={14}>
+            <Box w="100%" p={4}>
                 {children}
                 <Footer />
-            </Container>
+            </Box>
         </Box>
     );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,31 @@
-import { Box, Container, Heading, useColorModeValue } from "@chakra-ui/react";
+import { Box, Container, Heading, SimpleGrid, useColorModeValue } from "@chakra-ui/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
     return (
-        <Container>
-            <Box display={{ md: "flex" }}>
-                <Box flexGrow={1}>
-                    <Heading as="h2" variant="page-title">
-                        Business assistant
-                    </Heading>
-                    <p>By team MIKA | Martin, Mads, Ida, Kaare, Kasper & Anders</p>
+        <Box pt={50}>
+            <Container>
+                <Box display={{ md: "flex" }}>
+                    <Box flexGrow={1}>
+                        <Heading as="h2" variant="page-title">
+                            Business assistant
+                        </Heading>
+                        <p>By team MIKA | Martin, Mads, Ida, Kaare, Kasper & Anders</p>
+                    </Box>
                 </Box>
-            </Box>
-            <Box
-                borderRadius="lg"
-                mb={6}
-                p={3}
-                textAlign="center"
-                bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
-            >
-                This could be the first integration
-            </Box>
-        </Container>
+            </Container>
+            <SimpleGrid minChildWidth="200px" spacing="40px">
+                <Box
+                    borderRadius="lg"
+                    mb={6}
+                    p={3}
+                    textAlign="center"
+                    bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+                >
+                    This could be the first integration
+                </Box>
+            </SimpleGrid>
+        </Box>
     );
 };
 
