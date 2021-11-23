46658ff0d0f6e8e08666ead61249f217f644c06f
Ida <ichris18@student.aau.dk>
2021-11-23 11:10:08 +0100

    Fixed focus in add modal and made it possible to use enter to instead of clicking on done

--- src/components/time-smart/add-modal.tsx
+++ src/components/time-smart/add-modal.tsx
@@ -13,28 +13,49 @@ import {
 } from "@chakra-ui/react";
 
 function AddModal(props: { open: boolean; onClose: () => void }) {
+    const initialRef = React.useRef();
     const [name, setName] = useState("");
     const closing = () => {
         props.onClose();
         console.log(name);
     };
 
+    const handleKeypress = (e) => {
+        if (e.code === "Enter") {
+            closing();
+        }
+    };
+
     const title = "Enter what you want to call the new registration form";
     return (
-        <Modal data-testid={"addModal"} size={"2xl"} isOpen={props.open} onClose={props.onClose}>
+        <Modal
+            data-testid={"addModal"}
+            size={"2xl"}
+            isOpen={props.open}
+            onClose={props.onClose}
+            initialFocusRef={initialRef}
+        >
             <ModalOverlay />
             <ModalContent>
                 <ModalHeader data-testid={"modal-header"}>{title}</ModalHeader>
                 <ModalCloseButton />
                 <ModalBody>
                     <Input
+                        ref={initialRef}
                         role="user-input"
                         placeholder="E.g. Pause"
+                        onKeyDown={handleKeypress}
                         onChange={(event) => setName(event.currentTarget.value)}
                     />
                 </ModalBody>
                 <ModalFooter>
-                    <Button role={"done-button"} bg={useColorModeValue("pink", "teal")} mr={3} onClick={closing}>
+                    <Button
+                        type="submit"
+                        role={"done-button"}
+                        bg={useColorModeValue("pink", "teal")}
+                        mr={3}
+                        onClick={closing}
+                    >
                         Done
                     </Button>
                 </ModalFooter>
