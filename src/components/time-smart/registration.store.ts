54738ac53fadd3f10af08e4fc61003ece59958ad
Mads Smed <mthoma18@student.aau.dk>
2021-11-23 14:32:18 +0100

    Add registration store
    Signed-off-by: Mads Smed <mthoma18@student.aau.dk>

---
+++ src/components/time-smart/registration.store.ts
@@ -0,0 +1,28 @@
+import { atom, SetterOrUpdater } from "recoil";
+import { log } from "../../helpers/logger";
+
+export const categoriesState = atom({
+    key: "categories",
+    default: getCategoriesAPICall(),
+});
+
+export function getCategoriesAPICall(): string[] {
+    return ["Kvalitet", "Fejl", "Møde", "Pause"];
+}
+
+export function addCategory(state, newCategory: string) {
+    const [categories, setCategories]: [string[], SetterOrUpdater<string[]>] = state;
+    if (categories.some((cat) => cat === newCategory)) {
+        log("Cannot add category of same name!");
+        return;
+    }
+    new Promise<void>((resolve) => {
+        // This 'fakes' the API call
+        setTimeout(() => {
+            resolve();
+        }, 500);
+    }).then(() => {
+        setCategories([...categories, newCategory]);
+        console.log(newCategory);
+    });
+}
