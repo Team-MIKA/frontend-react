d2bca269736ed9014183f3536b399f6feb9c4970
Kaare Hattel <kaare.hattel@gmail.com>
2021-11-26 11:01:03 +0100

    Type on onClose fnc
    Co-authored-by: Anders Aaen Springborg <aaspringborg@gmail.com>

--- src/components/workspace/add-widget-modal.tsx
+++ src/components/workspace/add-widget-modal.tsx
@@ -20,7 +20,7 @@ const cards = [
     { title: "TABLE", id: "3" },
 ] as Card[];
 
-const AddWidgetModal = ({ onClose, isOpen }: { onClose: any; isOpen: boolean }) => {
+const AddWidgetModal = ({ onClose, isOpen }: { onClose: () => void; isOpen: boolean }) => {
     const [workspace, setWorkspace] = useRecoilState(WorkspaceState);
     const [selectedCard, setSelectedCard] = useState({} as Card);
 
