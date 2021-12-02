44d132bb51ee0c89e91b705269ae1c142c266e6f
Kaare Hattel <kaare.hattel@gmail.com>
2021-12-02 12:43:13 +0100

    Workspace state moved to workspace folder

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -4,24 +4,3 @@ export const GroupMemberState = atom({
     key: "groupMembers",
     default: [] as string[],
 });
-
-export interface Card {
-    title: string;
-    id: string;
-}
-
-export interface Workspace {
-    id: string;
-    title: string;
-    cards: Card[];
-}
-
-export const WorkspaceListState = atom({
-    key: "workspaces",
-    default: [] as Workspace[],
-});
-
-export const WorkspaceState = atom({
-    key: "workspace",
-    default: { id: "", title: "", cards: [] as Card[] } as Workspace,
-});
