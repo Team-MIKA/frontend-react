cb57770b324c1ff9460346c5a45ad8b601d8b96e
Kaare Hattel <kaare.hattel@gmail.com>
2021-12-02 17:54:03 +0100

    implemented order Id subscriptioon

--- src/components/widgets/time-smart/regitstration-buttons.tsx
+++ src/components/widgets/time-smart/regitstration-buttons.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { Button, SimpleGrid, useColorModeValue, useDisclosure } from "@chakra-ui/react";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { addRegistration, registrationsState } from "@components/widgets/time-smart-list/logged-registrations.store";
 import TimerModal from "@components/widgets/time-smart/timer-modal";
 import { log } from "@helpers/logger";
+import { publishId } from "@store/order";
 
 function RegistrationButtons({ buttons }: { buttons: string[] }) {
     const [buttonText, setButtonText] = useState("");
@@ -11,6 +12,9 @@ function RegistrationButtons({ buttons }: { buttons: string[] }) {
     const { isOpen, onOpen, onClose } = useDisclosure();
     let startTime = new Date();
     const state = useRecoilState(registrationsState);
+
+    const order = useRecoilValue(publishId);
+    log("order id", order);
 
     const opening = (buttonText: string) => {
         onOpen();
@@ -20,7 +24,7 @@ function RegistrationButtons({ buttons }: { buttons: string[] }) {
 
     const closing = () => {
         const stopTime = new Date();
-        const newReg = { category: buttonText, buttons, startTime: startTime, endTime: stopTime, orderId: "5" };
+        const newReg = { category: buttonText, buttons, startTime: startTime, endTime: stopTime, orderId: order.id };
         addRegistration(state, newReg);
         const registrationDuration = Math.floor((stopTime.getTime() - startTime.getTime()) / 1000);
         log(registrationDuration);
@@ -30,6 +34,7 @@ function RegistrationButtons({ buttons }: { buttons: string[] }) {
         <SimpleGrid minChildWidth="120px" spacing="20px">
             {buttons.map((buttonText) => (
                 <Button
+                    disabled={order.id === ""}
                     role={"reg-button"}
                     variant={"outline"}
                     bg={buttonColors}
