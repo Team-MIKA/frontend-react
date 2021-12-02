2779deeb7495e7173e93b7d5b155ea381b4a0e22
Kaare Hattel <kaare.hattel@gmail.com>
2021-12-02 17:53:28 +0100

    Implemented delete widget button

---
+++ src/components/workspace/delete-widget.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { IconButton } from "@chakra-ui/button";
+import { DeleteIcon } from "@chakra-ui/icons";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { HideOptionsState, WorkspaceState } from "@store/workspace";
+import { WorkspaceService } from "services/openapi/services/WorkspaceService";
+
+const DeleteWidget = ({ widgetId }: { widgetId: string }) => {
+    const hide = useRecoilValue(HideOptionsState);
+    const [workspace, setWorkspace] = useRecoilState(WorkspaceState);
+    const removeWidget = () => {
+        WorkspaceService.deleteWorkspace1(workspace.id, widgetId).then((result) => {
+            if (result) {
+                const filteredWidgets = workspace.widgets.filter((x) => x.id !== widgetId);
+                setWorkspace({ ...workspace, widgets: filteredWidgets });
+            }
+        });
+    };
+    return (
+        <>
+            <IconButton
+                hidden={hide}
+                textColor={"red"}
+                position={"absolute"}
+                background={"transparent"}
+                icon={<DeleteIcon />}
+                aria-label={"Remove Widget"}
+                onClick={() => removeWidget()}
+            />
+        </>
+    );
+};
+
+export default DeleteWidget;
