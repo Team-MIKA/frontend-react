4372c31f441dabe0a470435f1935089d1fe23184
Kaare Hattel <kaare.hattel@gmail.com>
2021-12-02 11:02:55 +0100

    Removed testing widgets and added timesmart widget

--- src/components/workspace/widget-render.tsx
+++ src/components/workspace/widget-render.tsx
@@ -1,13 +1,10 @@
+import TimeSmartCard from "@components/time-smart/time-smart-card";
 import { Card } from "@store/index";
-import WidgetSap from "./cards/widget-sap-example";
-import WidgetTimeSmart from "./cards/widget-time-smart-example";
 
 const WidgetRender = ({ card }: { card: Card }) => {
     switch (card.title) {
         case "TIME SMART":
-            return <WidgetTimeSmart card={card} />;
-        case "SAP":
-            return <WidgetSap card={card} />;
+            return <TimeSmartCard />;
         default:
             return null;
     }
