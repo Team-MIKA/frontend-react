897f7adc0ef56dab1d90e10b375d8cd9754d8a9a
Anders Aaen Springborg <aaspringborg@gmail.com>
2021-12-06 00:47:46 +0100

    back button

--- src/components/layouts/main.tsx
+++ src/components/layouts/main.tsx
@@ -1,7 +1,9 @@
-import { Box, Flex } from "@chakra-ui/react";
+import React from "react";
+import { Box, Center, Flex } from "@chakra-ui/react";
 import { NextPage } from "next";
 import Head from "next/head";
 import { Breadcrumbs } from "@components/Breadcrumbs";
+import { BackButton } from "@components/layouts/backButton";
 import { NavBarItem, TextNavItem } from "@components/layouts/navbar/navbaritem";
 import Footer from "./footer";
 import NavBar from "./navbar/navbar";
@@ -23,7 +25,14 @@ const Main: NextPage = ({ children }) => {
             <NavBar navItems={Items} />
 
             <Box w="100%" p={4} pt={20}>
-                <Breadcrumbs />
+                <Flex>
+                    <Box p={4}>
+                        <BackButton />
+                    </Box>
+                    <Center>
+                        <Breadcrumbs />
+                    </Center>
+                </Flex>
                 {children}
             </Box>
             <Footer />
