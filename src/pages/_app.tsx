d861bd11302149531326b47376a69f67c3bc4f8d
Anders Aaen Springborg <aaspringborg@gmail.com>
2021-12-06 16:51:23 +0100

    baseurl set in configprocess.env.NEXT_PUBLIC_API_URL

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import { ChakraProvider, SlideFade } from "@chakra-ui/react";
 import { AnimatePresence } from "framer-motion";
 import { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { setServerUrl } from "../backendServices";
 import Fonts from "../components/fonts";
 import Layout from "../components/layouts/main";
 import theme from "../components/layouts/theme";
 
 function Website({ Component, router, pageProps }: AppProps) {
+    setServerUrl(process.env.NEXT_PUBLIC_API_URL);
+
     return (
         <ChakraProvider theme={theme}>
             <RecoilRoot>
