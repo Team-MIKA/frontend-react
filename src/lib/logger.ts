903a489f0182181e5b7789d4ff132159724c1107
Anders Aaen Springborg <aaspringborg@gmail.com>
2021-12-09 14:13:10 +0100

    log from "dev" to "not production"

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,5 +1,5 @@
 export const log: (message?: any, ...optionalParams: any[]) => void =
-    process.env.NODE_ENV == "development" ? console.log : () => null;
+    process.env.NODE_ENV != "production" ? console.log : () => null;
 
 export const error = (message?: any, ...optionalParams: any[]) => {
     log("[ERROR]" + message, ...optionalParams);
